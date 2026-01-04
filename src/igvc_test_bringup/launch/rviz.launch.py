8b7865ea1a313665c92e5cc23f0374a3dfba63b5
nchan18 <nchan18@outlook.com>
2026-01-04 06:49:47 +0000

    added launch files

--- src/igvc_test_bringup/launch/rviz.launch.py
+++ src/igvc_test_bringup/launch/rviz.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch_ros.actions import Node
 
@@ -12,22 +12,17 @@ import xacro
 def generate_launch_description():
 
     # Check if we're told to use sim time
+    hardware_interface = LaunchConfiguration('hardware_interface')
     use_sim_time = LaunchConfiguration('use_sim_time')
 
     # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('grr_description'))
-    xacro_file = os.path.join(pkg_path,'urdf', 'robots','grreg.urdf.xacro')
-    grr_description_config = xacro.process_file(xacro_file)
-    source_code_path = os.path.abspath(os.path.join(pkg_path, "../../../../src/grr_description"))
-
-    urdf_save_path = os.path.join(source_code_path, "grreg.urdf")
-
-    with open(urdf_save_path, 'w') as f:
-        f.write(grr_description_config.toxml())
-    # Create a robot_state_publisher node
+    pkg_path = os.path.join(get_package_share_directory('igvc_test_description'))
+    xacro_file = os.path.join(pkg_path,'urdf','robots','test_robot.urdf.xacro')
+    # robot_description_config = xacro.process_file(xacro_file).toxml()
+    robot_description_config = Command(['xacro ', xacro_file, ' hardware_interface:=', hardware_interface])
     
     # Create a robot_state_publisher node
-    params = {'robot_description': grr_description_config.toxml(), 'use_sim_time': use_sim_time}
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -42,13 +37,18 @@ def generate_launch_description():
     )
 
     # Start Rviz2 with basic view
-    rviz2_config_path = os.path.join(get_package_share_directory('grr_bringup'), 'config/rviz.rviz')
+    rviz2_config_path = os.path.join(get_package_share_directory('igvc_test_bringup'), 'config/config.rviz')
     run_rviz2 = ExecuteProcess(
         cmd=['rviz2', '-d', rviz2_config_path],
         output='screen'
     )
     # Launch!
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'hardware_interface',
+            default_value='IsaacDriveHardware',
+            description='Define what hardware interface to use'),
+        
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='true',
