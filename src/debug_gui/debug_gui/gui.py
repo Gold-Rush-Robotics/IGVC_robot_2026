9ca205f1882090563b9887187a997ffc003d0a07
nchan18 <nchan18@outlook.com>
2026-05-18 00:47:03 -0400

    test cases

--- src/debug_gui/debug_gui/gui.py
+++ src/debug_gui/debug_gui/gui.py
@@ -30,8 +30,8 @@ class JointStatePublisher(Node):
         self.image_signal.signal.connect(self.app.update_image)
 
     def create_subscribers(self):
-        # topics = [ "left_zed_camera_x/zed_node/rgb/color/rect/image","/front_zed_camera_x/zed_node/rgb/color/rect/image","right_zed_camera_x/zed_node/rgb/color/rect/image"]
-        topics = ["/right_zed_camera_x/zed_node/confidence/confidence_map", "/lane_debug/cam0/overlay", "/lane_debug/cam1/overlay", "/lane_debug/cam2/overlay" ]
+        topics = [ "left_zed_camera_x/zed_node/rgb/color/rect/image","/front_zed_camera_x/zed_node/rgb/color/rect/image","right_zed_camera_x/zed_node/rgb/color/rect/image"]
+        # topics = ["/right_zed_camera_x/zed_node/confidence/confidence_map", "/lane_debug/cam0/overlay", "/lane_debug/cam1/overlay", "/lane_debug/cam2/overlay" ]
         # topics = [ "/lane_debug/cam0/overlay" ]
         # topics = [ "/debug/stitched_lanes" ]
         for i, topic in enumerate(topics):
