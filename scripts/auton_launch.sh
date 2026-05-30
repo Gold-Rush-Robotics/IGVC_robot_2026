0706bf0cff66db5708a2b57ef9c8eef140006690
nchan18 <nchan18@outlook.com>
2026-05-30 13:22:06 -0400

    fies

--- scripts/auton_launch.sh
+++ scripts/auton_launch.sh
@@ -4,5 +4,7 @@ docker start dazzling_easley
 docker exec -it dazzling_easley /bin/bash -c "
 cd src/IGVC_robot_2026;
 source install/setup.bash;
+export PYTHONPATH=$PYTHONPATH:/opt/venv/lib/python3.12/site-packages;
+export YOLOPV2_WEIGHTS=/root/ros2_ws/src/IGVC_robot_2026/models/yolopv2.pt
 ros2 launch igvc_test_bringup igvc_fused_drive.launch.py;
 "
