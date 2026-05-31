a23a3c3a5b5e36865e11193fe986841560f2b3e4
nchan18 <nchan18@outlook.com>
2026-05-31 10:34:57 -0400

    path

--- src/igvc_imu_interface/setup.py
+++ src/igvc_imu_interface/setup.py
@@ -24,7 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'imu_interface_node = igvc_imu_interface.imu_interface_node:main',
+            'imu_interface_node = igvc_imu_interface.l3gd20_heading_node:main',
         ],
     },
 )
